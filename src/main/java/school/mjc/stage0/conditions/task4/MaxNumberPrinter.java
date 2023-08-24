f85e8879b3f485448216de62cb5d95c3b8c014d9
Martín Alcalá <carlos_a_v_bm@hotmail.com>
2023-08-23 19:38:19 -0600

    Ternary Operator´s implemented

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int fourth;
+        fourth = first > second ? first : second;
+        fourth = fourth > third ? fourth: third;
+        System.out.println(fourth);
+
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int fourth;
+        fourth = first > second ? first : second;
+        System.out.println(fourth > third ? fourth: third);
     }
 }
